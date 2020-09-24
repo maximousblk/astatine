267eeab02193691eca8a0687e6ba0ce5ea0671e3
tate <tateflyer1@gmail.com>
2020-09-24 19:51:28 -0400

    feat: cannon priming & creation of token allocation function

--- config.js
+++ config.js
@@ -6,6 +6,9 @@ const config = {
     name: "linear",
     distribution_slope: "",   // Pick one or the other
     initial_emit_amount: "",  // Pick one or the other to fill in
+  },
+  taf: () => {
+    return [];
   }
 };
 
--- index.js
+++ index.js
@@ -1,22 +1,64 @@
 const Arweave = require("arweave");
 const config = require("./config");
+const jwk = "";     // MAX - WE NEED TO PULL THIS FROM THE GITHUB SECRET
 
-const arweave = Arweave.init({
-  host: 'arweave.net',
-  port: 443,
-  protocol: 'https'
-});
-
+/*
+Get the current time in relation to when the cannon was started.
+**/
 function getTime() {
 
   return "position on x axis"
 }
 
-function emit(amount) {
- // Creates txs & sends them
+/*
+Generate all transactions necessary to emit.
+**/
+function primeCannon(amount, addresses, time) {
+  const
+    arweave = Arweave.init({
+      host: 'arweave.net',
+      port: 443,
+      protocol: 'https'
+    }),
+    userGets = Math.floor(amount / addresses.length);
+  let allTransactions = [];
+  for (let i = 0; i < addresses.length; i++) {
+    const tags = {
+      Cannon: "PST",
+      Function: config.emit_curve.name,
+      Completion: time / config.emit_total_time * 100,
+      Contract: config.token_contract_id,
+      "App-Name": "SmartWeaveAction",
+      "App-Version": "0.3.0",
+      Input: JSON.stringify({
+        function: "transfer",
+        target: addresses[i],
+        qty: userGets
+      })
+    };
+    const tx = await arweave.createTransaction({
+      target: addresses[i],
+      data: Math.random().toString().slice(-4),
+    }, jwk);
+    for (const [key, value] of Object.entries(tags)) {
+      tx.addTag(key, value.toString());
+    }
+    await arweave.transactions.sign(tx, jwk);
+    allTransactions.push(tx);
+  }
+  return allTransactions;
 }
 
-// Distribute tokens on a linear graph
+/*
+Send all of the transactions to the corresponding addresses.
+**/
+function emit(transactions) {
+
+}
+
+/*
+Distribute tokens on a linear decreasing function.
+**/
 function linear(time) {
   let
     distributionSlope = config.emit_curve.distribution_slope,
@@ -36,11 +78,13 @@ function linear(time) {
 }
 
 const time = getTime();
+let amount;
 
 if (config.emit_curve.name === "linear") {
-  let tx = linear(time);
-  let amount = 0
-  emit(amount);
+  amount = linear(time);
 }
 
-
+if (amount) {
+  let transactions = primeCannon(amount, config.taf, time);
+  emit(transactions);
+}
