ff138958de77dae8c0638052a2a7d656de7a58fc
Maximous Black <maximousblk@gmail.com>
2020-09-24 17:14:35 +0530

    but then i realised

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+  token: "blah-blah", // token id
+  emmition: "100000", // amount of tokens to emit
+  half_life: "31536000", // time period in seconds
+  paf: () => {}, // point allocation function
+};
