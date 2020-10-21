219939b80e77285de0f140c4ed91b091dc3d1a13
Tate Berenbaum <tateflyer1@gmail.com>
2020-10-21 19:27:32 +0000

    chore: change config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
   token_contract_id: "fE2OcfjlS-sHqG5K8QvxE8wHtcqKxS-YV0bDEgxo-eI", // token id
-  emission_period: 300,        // E
-  time_interval: 60,           // I
+  emission_period: 600,        // E
+  time_interval: 300,           // I
   initial_emit_amount: 300,    // A
-  decay_const: 0.02,           // k
+  decay_const: 0.0025,           // k
   taf: ["-zkZpuG7DiIsdFzRVgWdLQ3zxU2bDY-0r90Ri8lxL-A"]
 };
 
