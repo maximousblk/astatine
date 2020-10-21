3bf5a29e7e59a99d5a89b6fe0b6dad6a30f427b7
Tate Berenbaum <tateflyer1@gmail.com>
2020-10-21 18:24:58 +0000

    chore: configure variables

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const config = {
-  token_contract_id: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", // token id
-  emission_period: 31536000, // E
-  time_interval: 1,          // I
-  initial_emit_amount: 0,    // A
-  decay_const: 0.1,          // k
+  token_contract_id: "fE2OcfjlS-sHqG5K8QvxE8wHtcqKxS-YV0bDEgxo-eI", // token id
+  emission_period: 300,        // E
+  time_interval: 60,           // I
+  initial_emit_amount: 300,    // A
+  decay_const: 0.02,           // k
   taf: () => {
-    return [];
+    return ["-zkZpuG7DiIsdFzRVgWdLQ3zxU2bDY-0r90Ri8lxL-A"];
   },
 };
 
