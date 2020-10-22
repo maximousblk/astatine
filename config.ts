d89af009c8df0f3d88673bcb8591abbe44d961eb
Maximous Black <maximousblk@gmail.com>
2020-10-22 10:05:23 +0000

    typescript
    and merge status and distribution files

---
+++ config.ts
@@ -0,0 +1,14 @@
+import { config } from "./index";
+
+const token_allocation_function: () => string[] = () => [];
+
+const config: config = {
+    emission_period: 600, // E
+    time_interval: 300, // I
+    initial_emit_amount: 300, // A
+    decay_const: 0.0025, // k
+    token_contract_id: "fE2OcfjlS-sHqG5K8QvxE8wHtcqKxS-YV0bDEgxo-eI",
+    token_allocations: ["-zkZpuG7DiIsdFzRVgWdLQ3zxU2bDY-0r90Ri8lxL-A"],
+};
+
+export default config;
