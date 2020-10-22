4fe69c10e942370a4c4b5e7f512ba4233179e06e
Maximous Black <maximousblk@gmail.com>
2020-10-22 11:51:13 +0000

    update config

--- config.ts
+++ config.ts
@@ -3,10 +3,10 @@ import { config } from './index';
 const token_allocation_function: () => string[] = () => [];
 
 const config: config = {
-  emission_period: 600, // E
-  time_interval: 300, // I
+  emission_period: 1800, // E
+  time_interval: 900, // I
   initial_emit_amount: 300, // A
-  decay_const: 0.0025, // k
+  decay_const: 0.0005, // k
   token_contract_id: 'fE2OcfjlS-sHqG5K8QvxE8wHtcqKxS-YV0bDEgxo-eI',
   token_allocations: ['-zkZpuG7DiIsdFzRVgWdLQ3zxU2bDY-0r90Ri8lxL-A'],
 };
